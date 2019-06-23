389b14469ce71b762efaadb0d1894ca328cbf533
mori <tk011875@gmail.com>
2019-06-23 17:57:12 +0900

    add:record img file list(./img/cap.list)

--- camera.py
+++ camera.py
@@ -2,17 +2,17 @@ import cv2
 from datetime import datetime
 
 
-def capture_camera(mirror=True, size=None):
-    """Capture video from camera"""
-    # カメラをキャプチャする
-    cap = cv2.VideoCapture(0) # 0はカメラのデバイス番号
+CAP_DIR = "./img/"
 
-    # retは画像を取得成功フラグ
-    ret, frame = cap.read()
+
+def capture_camera(file_path, size=None):
+    cap = cv2.VideoCapture(0)#-1)  # カメラのデバイス番号
+    cap.read()  # 初回のキャプチャは失敗する.
+    ret, frame = cap.read()  # retは画像を取得成功フラグ
 
     # 鏡のように映るか否か
-    if mirror is True:
-        frame = frame[:,::-1]
+    # if mirror is True:
+    #     frame = frame[:, ::-1]
 
     # フレームをリサイズ
     # sizeは例えば(800, 600)
@@ -21,14 +21,23 @@ def capture_camera(mirror=True, size=None):
 
     # フレームを表示する
     # cv2.imshow('camera capture', frame)
-    date = datetime.now().strftime("%Y%m%d_%H%M%S")
-    path = "./img/" + date + ".png"
-    cv2.imwrite(path, frame)  # ファイル保存
+    cv2.imwrite(file_path, frame)  # ファイル保存
 
     # キャプチャを解放する
     cap.release()
     cv2.destroyAllWindows()
 
+    record_capture(file_path)
+
+
+def record_capture(cap_file_path):
+    cap_list_path = CAP_DIR+"cap.list"
+
+    with open(cap_list_path, mode='a') as f:
+        f.write(cap_file_path+"\n")
+
 
 if __name__ == "__main__":
-    capture_camera()
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = CAP_DIR + date + ".png"
+    capture_camera(file_path)
