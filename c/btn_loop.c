126706972454ec33bc9deb366336bee0efc8cdcc
Masahiro Nakamura <13937915+tsuu32@users.noreply.github.com>
2019-06-24 17:47:03 +0900

    add: work around key chattering

--- c/btn_loop.c
+++ c/btn_loop.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-int SW_PIN[] = {0, 7};  //TODO:ボタンのピンの確認・ボタンは３つ
-int SW_FLG[] = {0, 0};
+int SW_PIN[] = {7, 0, 2};  //TODO:ボタンのピンの確認・ボタンは３つ
+int SW_FLG[] = {0, 0, 0};
 char *btn0_cmd = "python3 -V";  //とりあえずPythonを呼び出せるか確認
-char *btn1_cmd = "python2 -v";
+char *btn1_cmd = "python2 -V";
+char *btn2_cmd = "gcc --version";
 int res;
 
 
@@ -17,11 +18,15 @@ int main(int argc, char *argv[]) {
 
     pinMode(SW_PIN[0], INPUT);
     pinMode(SW_PIN[1], INPUT);
-    
-    
-    printf("%d\n", digitalRead(SW_PIN[0]));
-    printf("%d\n", digitalRead(SW_PIN[1]));
+    pinMode(SW_PIN[2], INPUT);
 
+    pullUpDnControl(SW_PIN[0], PUD_DOWN);
+    pullUpDnControl(SW_PIN[1], PUD_DOWN);
+    pullUpDnControl(SW_PIN[2], PUD_DOWN);
+
+    printf("PIN{0]: %d\n", digitalRead(SW_PIN[0]));
+    printf("PIN[1]: %d\n", digitalRead(SW_PIN[1]));
+    printf("PIN[2]: %d\n", digitalRead(SW_PIN[2]));
 
     while(1) {
         //TODO:チャタリング除去
@@ -32,28 +37,59 @@ int main(int argc, char *argv[]) {
         // 1. SW_FLG（フラグ）でなく, 押した時の時間を取得し保存
         // 2. ボタン押下時に, 保存した時刻からN秒経過しているか確認
         // 3. 経過していればbtn*_cmd()を実行
+        /* btn0 */
         if (SW_FLG[0] == 0 && digitalRead(SW_PIN[0]) == 1 ) {
-            printf("SW:0 is ON\n");
-            SW_FLG[0] = 1;
+            delay(5);
+            if (digitalRead(SW_PIN[0]) == 1 ) {
+                printf("SW:0 is ON\n");
+                SW_FLG[0] = 1;
 
-            res = system(btn0_cmd);
-            printf("%d\n", WIFEXITED(res));
-        }else if(SW_FLG[0] == 1 && digitalRead(SW_PIN[0]) == 0) {
+                printf("------------------------------------\n");
+                res = system(btn0_cmd);
+                printf("------------------------------------\n");
+                printf("%d\n", WIFEXITED(res));
+                delay(1000);
+            }
+        } else if(SW_FLG[0] == 1 && digitalRead(SW_PIN[0]) == 0) {
             printf("SW:0 is OFF\n");
             SW_FLG[0] = 0;
         }
 
+        /* btn1 */
         if (SW_FLG[1] == 0 && digitalRead(SW_PIN[1]) == 1 ) {
-            printf("SW:0 is ON");
-            SW_FLG[1] = 1;
+            delay(5);
+            if (digitalRead(SW_PIN[1]) == 1 ) {
+                printf("SW:1 is ON\n");
+                SW_FLG[1] = 1;
 
-            res = system(btn1_cmd);
-            printf("%d\n", WIFEXITED(res));
+                printf("------------------------------------\n");
+                res = system(btn1_cmd);
+                printf("------------------------------------\n");
+                printf("%d\n", WIFEXITED(res));
+                delay(1000);
+            }
         } else if(SW_FLG[1] == 1 && digitalRead(SW_PIN[1]) == 0) {
-            printf("SW:0 is OFF");
+            printf("SW:1 is OFF\n");
             SW_FLG[1] = 0;
+        }
+
+        /* btn2 */
+        if (SW_FLG[2] == 0 && digitalRead(SW_PIN[2]) == 1 ) {
+            delay(5);
+            if (digitalRead(SW_PIN[2]) == 1 ) {
+                printf("SW:2 is ON\n");
+                SW_FLG[2] = 1;
+
+                printf("------------------------------------\n");
+                res = system(btn2_cmd);
+                printf("------------------------------------\n");
+                printf("%d\n", WIFEXITED(res));
+                delay(1000);
+            }
+        } else if(SW_FLG[2] == 1 && digitalRead(SW_PIN[2]) == 0) {
+            printf("SW:2 is OFF\n");
+            SW_FLG[2] = 0;
         }
     }
     return 0;
 }
-
